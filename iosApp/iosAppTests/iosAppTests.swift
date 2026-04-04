5bf027b39ee35c1ac0770f5e96c8becceb8761e9
Riya Swami <riswami@expediagroup.com>
2026-04-04 23:39:23 +0530

    test(ios): unit tests and repository mock

---
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -0,0 +1,25 @@
+//
+//  iosAppTests.swift
+//  iosAppTests
+//
+//  Created by Riya Swami on 02/04/26.
+//
+
+import XCTest
+@testable import iosApp
+
+final class iosAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+
+}
