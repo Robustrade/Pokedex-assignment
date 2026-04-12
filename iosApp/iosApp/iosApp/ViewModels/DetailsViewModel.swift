5d95e2a09e6a993c4a0252559164d98a13b53446
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2026-04-12 23:05:29 +0530

    added details view model

---
+++ iosApp/iosApp/iosApp/ViewModels/DetailsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  DetailsViewModel.swift
+//  iosApp
+//
+//  Created by Akash K on 12/04/26.
+//
+
+import shared
+import Combine
+
+final class DetailsViewModel: ObservableObject {
+    @Published private(set) var detailState: PokemonDetailState = .Loading()
+    private let viewModel: PokemonDetailViewModel
+    private var stateTask: Task<Void, Never>?
+    
+    init(name: String, repository: PokemonRepository) {
+        viewModel = PokemonDetailViewModel(
+            pokemonName: name,
+            repository: repository
+        )
+        
+        if let currentState = viewModel.state.value as? PokemonDetailState {
+            detailState = currentState
+        }
+        
+        observeState()
+    }
+    
+    deinit {
+        viewModel.onCleared()
+        stateTask?.cancel()
+    }
+    
+    private func observeState() {
+        let stateFlow = viewModel.state
+        
+        stateTask = Task { [weak self] in
+            for await state in stateFlow.stream(of: PokemonDetailState.self) {
+                guard let self else { return }
+                await MainActor.run {
+                    self.detailState = state
+                }
+            }
+        }
+    }
+}
