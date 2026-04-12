c82d6919f9e898e2b5f9bbffbfb27207816008db
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2026-04-12 12:30:13 +0530

    initial setup
    added xcode project.
    created koinRepository manager

---
+++ iosApp/iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,23 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Akash K on 12/04/26.
+//
+
+import SwiftUI
+import shared
+
+@main
+struct iosAppApp: App {
+    
+    init() {
+        _ = KoinDependencyManager.shared
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
